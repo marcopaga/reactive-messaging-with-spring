ec2b5e7593aea7ce5159ba178144de469cfaa56e
Marco Paga <marco.paga@codecentric.de>
2018-05-18 14:24:43 +0200

    WebFlux

---
+++ backend/src/main/java/de/codecentric/qxd/backend/controllers/Message.java
@@ -0,0 +1,18 @@
+package de.codecentric.qxd.backend.controllers;
+
+public class Message {
+
+  String message;
+
+  public Message(final String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(final String message) {
+    this.message = message;
+  }
+}
--- backend/src/main/java/de/codecentric/qxd/backend/controllers/WorkerController.java
+++ backend/src/main/java/de/codecentric/qxd/backend/controllers/WorkerController.java
@@ -1,10 +1,14 @@
 package de.codecentric.qxd.backend.controllers;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.codecentric.qxd.backend.mq.WorkerMessaging;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class WorkerController {
@@ -19,5 +23,9 @@ public class WorkerController {
   }
 
 
+   @GetMapping(value = "/sse", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+   public Flux<Message> serverSendEvents(){
+     return Flux.interval(Duration.ofSeconds(1)).map( bla -> new Message("Hallo"));
+   }
 
 }
