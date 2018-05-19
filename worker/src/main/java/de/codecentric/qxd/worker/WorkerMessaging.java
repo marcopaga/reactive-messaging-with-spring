83d790e03c7be66c7e5fe30cbf18db567aaae48f
Marco Paga <marco.paga@codecentric.de>
2018-05-19 07:50:42 +0200

    Switched to reactive API

--- worker/src/main/java/de/codecentric/qxd/worker/WorkerMessaging.java
+++ worker/src/main/java/de/codecentric/qxd/worker/WorkerMessaging.java
@@ -3,12 +3,14 @@ package de.codecentric.qxd.worker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 import de.codecentric.qxd.Message;
+import reactor.core.publisher.Flux;
 
 @Component
 @EnableBinding(Processor.class)
@@ -16,11 +18,11 @@ public class WorkerMessaging {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(WorkerMessaging.class);
 
-  @StreamListener(Processor.INPUT)
+  @StreamListener
   @SendTo(Processor.OUTPUT)
-  public Message onPing(Message message) {
-    LOGGER.info("{} received. Sending pong", message);
-    return new Message("pong");
+  public Flux<Message> onPing(@Input(Processor.INPUT) Flux<Message> message) {
+    LOGGER.info("Worker message pipeline set up");
+    return message.map( input -> new Message("Pong for " + input.getMessage()));
   }
 
 }
